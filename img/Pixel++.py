9e9457c583c575d1c4d4ff9e1429166c4db02a24
Team-Pixel-Yao <141705896+Team-Pixel-Yao@users.noreply.github.com>
2023-08-28 14:13:43 +0800

    Upload by PicGo

---
+++ img/Pixel++.py
@@ -0,0 +1,13 @@
+import turtle
+from MIT_licence import mit
+print("Pixel++ 1.0.0    (Powered by Python  3.x)")
+print("Made by Pixel Team-Yao Xingyu")
+print("开发者不承担使用本软件时违反使用条例造成的法律责任,其包括非法使用爬虫、SQL注入等工具.")
+print("若要发布经修改后的代码，请输入/mit_licence查看许可证.")
+print("-------------------------------------------Pixel++ 1.0.0(School version)-------------------------------------------")
+print("Please input your command:",end='')
+command=input()
+if command == "/mit_licence":
+    mit()
+print("Press any key to break...")
+quit=input()
